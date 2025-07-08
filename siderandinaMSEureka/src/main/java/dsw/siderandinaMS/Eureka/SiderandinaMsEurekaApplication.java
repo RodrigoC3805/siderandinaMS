96428facc773e4caafabfc0daff1bd907d1a2271
nxn24 <nisavi.18@gmail.com>
2025-07-08 13:59:06 -0500

    resolv conflicts in pom global and pom eureka

--- siderandinaMSEureka/src/main/java/dsw/siderandinaMS/Eureka/SiderandinaMsEurekaApplication.java
+++ siderandinaMSEureka/src/main/java/dsw/siderandinaMS/Eureka/SiderandinaMsEurekaApplication.java
@@ -2,6 +2,8 @@ package dsw.siderandinaMS.Eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @EnableEurekaServer
 @SpringBootApplication
