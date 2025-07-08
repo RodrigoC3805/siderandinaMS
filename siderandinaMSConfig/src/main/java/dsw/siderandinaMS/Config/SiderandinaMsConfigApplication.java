3ca9eb11ddf704ada4875160d8f7fcd027ced451
nxn24 <nisavi.18@gmail.com>
2025-07-08 16:03:22 -0500

    add MSConfig

---
+++ siderandinaMSConfig/src/main/java/dsw/siderandinaMS/Config/SiderandinaMsConfigApplication.java
@@ -0,0 +1,14 @@
+package dsw.siderandinaMS.Config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SiderandinaMsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiderandinaMsConfigApplication.class, args);
+	}
+
+}
