a90b0797b6c90353e01b4a35dc18c31a87a58b11
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2025-07-08 22:38:22 -0500

    added MS RRHH

---
+++ siderandinaMSRRHH/src/test/java/dsw/siderandinaMS/RRHH/SiderandinaMsrrhhApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw.siderandinaMS.RRHH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiderandinaMsrrhhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
